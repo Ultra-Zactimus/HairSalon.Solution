eb80c0dc6ad5abe77c225ef9b45fd58a172f273b
Ultra Zacky <ultrazacky@gmail.com>
2022-03-11 09:23:20 -0800

    add code for Stylist and Client classes

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,4 +1,12 @@
 namespace Salon.Models
 {
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string ClientName { get; set; }
+    public int PhoneNumber { get; set; }
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
 
+  }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,5 +2,14 @@ using System.Collections.Generic;
 
 namespace Salon.Models
 {
-
+    public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
 }
